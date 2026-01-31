3043e26877ff0cfeab8b57fb689d35d0086f02f8
YuvrajKashid05 <yuvraj.kashid03@gmail.com>
2026-01-31 11:36:45 +0530

    url issue updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://todo-app-omega-ashen.vercel.app"
+            "https://todo-app-omega-ashen.vercel.app/"
         ],
         credentials: true,
     })
