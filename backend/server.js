fb4e150e1a5b841e0840f0a33b3689666e5ac36c
YuvrajKashid05 <yuvraj.kashid03@gmail.com>
2026-01-31 11:28:35 +0530

    backend url update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://todo-app-omega-ashen.vercel.app"
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
